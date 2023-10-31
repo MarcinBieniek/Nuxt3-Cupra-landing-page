fad95f566d6dde36e6db1d345fca9809bb97c8d3
Marcin Bieniek <marcingim@gmail.com>
2023-10-31 22:40:45 +0100

    chore: organise folders structure, add dependencies & setup config file

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  srcDir: "src/",
+  ssr: false,
+  devtools: { enabled: true },
+  css: ['@/assets/css/main.scss', 'animate.css/animate.min.css'],
+  modules: ['nuxt-swiper', 'nuxt-icon', '@nuxt/image'],
+  image: {
+    dir: 'assets/images'
+  },
 })
--- src/app.vue
+++ src/app.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
     <NuxtWelcome />
+    asd
   </div>
 </template>
